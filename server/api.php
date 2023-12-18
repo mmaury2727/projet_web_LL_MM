09153291abbcc3e2aee35635cd46acd51e9e018b
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-12-18 17:30:42 +0100

    deployment fixes

--- server/api.php
+++ server/api.php
@@ -1,11 +1,11 @@
 <?php
 header("Content-Type: application/json");
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 // Database credentials
-$servername = "localhost";
+$servername = "db";
 $username = "root";
 $password = "";
 $dbname = "hendektracker";
